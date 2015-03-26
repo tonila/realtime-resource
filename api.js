a56b63b1e69d41540ee7a2212e3ebb779a353ea1
Toni Lahnalampi <toni.lahnalampi@livion.fi>
2015-03-26 19:10:14 +0000

    place code inside UMD

--- api.js
+++ api.js
@@ -1,65 +1,3 @@
-'use strict';
-/* global define*/
-var url = 'http://localhost:3000/';
-//var EventEmitter = require('event-emitter');
-var io = require('socket.io-client');
-
-function RealRecord(name, data) {
-  this._shallowClearAndCopy = function(src, des) {
-    for (var key in src) {
-      var o = src[key];
-      if (src.hasOwnProperty(key) &&
-        typeof o !== 'object' &&
-        typeof o !== 'function' &&
-        (key === '_id' ||
-        key.charAt(0) !== '_')) {
-        des[key] = o;
-      }
-    }
-  }
-  this._name = name;
-  this._shallowClearAndCopy(data || {}, this);
-  this._socket = io.connect(url + this._name);
-}
-
-RealRecord.prototype.save = function (callback) {
-  var data = {};
-  this._shallowClearAndCopy(this, data);
-  this._socket.emit('save', data, function(err, res) {
-    if (callback) {
-      callback(err, res);
-    }
-  });
-}
-
-RealRecord.prototype.get = function (data, callback) {
-  var req = this;
-  this._socket.emit('get', data, function(err, res) {
-    if (callback) {
-      var result;
-      if (typeof res === 'object') {
-        result = [];
-        for (var i = 0, len = res.length; i < len; i++) {
-          result.push(new RealRecord(req._name, res[i]));
-        }
-      }
-      callback(err, result);
-    }
-  });
-};
-
-//RealRecord.prototype = new EventEmitter();
-
-/*
-function Api() {
-  var api = this;
-  api.createRecord = function(name) {
-    return new RealRecord(name);
-  }
-}
-Api.prototype = new EventEmitter();
-*/
-
 // UMD pattern
 // https://github.com/umdjs/umd
 (function(root, factory) {
@@ -77,5 +15,66 @@ Api.prototype = new EventEmitter();
   }
   /* jshint validthis: true */
 }(this, function() {
+  'use strict';
+  /* global define*/
+  var url = 'http://localhost:3000/';
+  //var EventEmitter = require('event-emitter');
+  var io = require('socket.io-client');
+
+  function RealRecord(name, data) {
+    this._shallowClearAndCopy = function(src, des) {
+      for (var key in src) {
+        var o = src[key];
+        if (src.hasOwnProperty(key) &&
+            typeof o !== 'object' &&
+            typeof o !== 'function' &&
+            (key === '_id' ||
+             key.charAt(0) !== '_')) {
+          des[key] = o;
+        }
+      }
+    }
+    this._name = name;
+    this._shallowClearAndCopy(data || {}, this);
+    this._socket = io.connect(url + this._name);
+  }
+
+  RealRecord.prototype.save = function (callback) {
+    var data = {};
+    this._shallowClearAndCopy(this, data);
+    this._socket.emit('save', data, function(err, res) {
+      if (callback) {
+        callback(err, res);
+      }
+    });
+  }
+
+  RealRecord.prototype.get = function (data, callback) {
+    var req = this;
+    this._socket.emit('get', data, function(err, res) {
+      if (callback) {
+        var result;
+        if (typeof res === 'object') {
+          result = [];
+          for (var i = 0, len = res.length; i < len; i++) {
+            result.push(new RealRecord(req._name, res[i]));
+          }
+        }
+        callback(err, result);
+      }
+    });
+  };
+
+  //RealRecord.prototype = new EventEmitter();
+
+  /*
+function Api() {
+  var api = this;
+  api.createRecord = function(name) {
+    return new RealRecord(name);
+  }
+}
+Api.prototype = new EventEmitter();
+*/
   return RealRecord;
 }));
