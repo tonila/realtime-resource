515abc98334d6232f22ed39a02f780427ea1c327
Toni Lahnalampi <toni.lahnalampi@livion.fi>
2015-03-28 10:46:06 +0200

    clean

--- server.js
+++ server.js
@@ -7,27 +7,10 @@ if (process.env.NODE_ENV !== 'production') {
   }
 }
 
-function handler (/*req, res*/) {
-  /*fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200);
-    res.end(data);
-  });*/
-}
+function handler (/*req, res*/) {}
 
 var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
-/*io.on('connection', function (socket) {
-  console.log('connection');
-  socket.on('create', function (data) {
-    console.log('create');
-    db.save('collection', data);
-  });
-});*/
 function createListener(name) {
   io.of(name)
   .on('connection', function (socket) {
