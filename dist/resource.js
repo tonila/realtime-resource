a0141e7a5aa0b6ee87d07af077d1709776561c24
Toni Lahnalampi <toni.lahnalampi@livion.fi>
2015-04-03 23:01:59 +0300

    build

--- dist/resource.js
+++ dist/resource.js
@@ -7001,32 +7001,30 @@ module.exports=require('xvZeu7');
 'use strict';
 
 var url = 'http://localhost:3000/';
-//var EventEmitter = require('event-emitter');
 var io = require('socket.io-client');
 
 function Record(name, socket, data) {
-  this._shallowClearAndCopy = function(src, des) {
+  this.$shallowClearAndCopy = function(src, des) {
     for (var key in src) {
       var o = src[key];
       if (src.hasOwnProperty(key) &&
           typeof o !== 'object' &&
           typeof o !== 'function' &&
-          (key === '_id' ||
-           key.charAt(0) !== '_')) {
+          key.charAt(0) !== '$') {
         des[key] = o;
       }
     }
   }
-  this._name = name;
-  this._shallowClearAndCopy(data || {}, this);
-  this._socket = socket;
+  this.$name = name;
+  this.$shallowClearAndCopy(data || {}, this);
+  this.$socket = socket;
 }
 
 Record.prototype.save = function (callback) {
   var rec = this;
   var data = {};
-  this._shallowClearAndCopy(this, data);
-  this._socket.emit('save', data, function(err, res) {
+  this.$shallowClearAndCopy(this, data);
+  this.$socket.emit('save', data, function(err, res) {
     rec._id = res._id;
     if (callback) {
       callback(err, res);
@@ -7038,7 +7036,7 @@ Record.prototype.remove = function (callback) {
   if (!this._id) {
     throw "can't remove record without id";
   }
-  this._socket.emit('remove', this._id, function(err, res) {
+  this.$socket.emit('remove', this._id, function(err, res) {
     if (callback) {
       callback(err, res);
     }
@@ -7049,15 +7047,15 @@ Record.prototype.remove = function (callback) {
 
 function Resource(name) {
   var reso = this;
-  var l = reso._listeners = {save:[], remove:[]};
-  reso._name = name;
-  reso._socket = io.connect(url + reso._name);
-  reso._socket.on('save', function(err, data) {
+  var l = reso.$listeners = {save:[], remove:[]};
+  reso.$name = name;
+  reso.$socket = io.connect(url + reso.$name);
+  reso.$socket.on('save', function(err, data) {
     for (var i = 0, len = l.save.length; i < len; i++) {
       l.save[i](err, reso.create(data));
     }
   });
-  reso._socket.on('remove', function(err, id) {
+  reso.$socket.on('remove', function(err, id) {
     for (var i = 0, len = l.remove.length; i < len; i++) {
       l.remove[i](err, id);
     }
@@ -7066,13 +7064,13 @@ function Resource(name) {
 
 Resource.prototype.get = function (data, callback) {
   var reso = this;
-  this._socket.emit('get', data, function(err, res) {
+  this.$socket.emit('get', data, function(err, res) {
     if (callback) {
       var result;
       if (typeof res === 'object') {
         result = [];
         for (var i = 0, len = res.length; i < len; i++) {
-          result.push(new Record(reso._name, reso._socket, res[i]));
+          result.push(new Record(reso.$name, reso.$socket, res[i]));
         }
       }
       callback(err, result);
@@ -7081,18 +7079,18 @@ Resource.prototype.get = function (data, callback) {
 };
 
 Resource.prototype.create = function (data) {
-  return new Record(this._name, this._socket, data);
+  return new Record(this.$name, this.$socket, data);
 };
 
 Resource.prototype.on = function (name, callback) {
   if (name !== 'save' && name !== 'remove') {
     throw 'listener ' + name + ' is not supported';
   }
-  this._listeners[name].push(callback);
+  this.$listeners[name].push(callback);
 };
 
 Resource.prototype.removeListener = function (name, fn) {
-  var l = this._listeners[name];
+  var l = this.$listeners[name];
   if (!l) {
     return;
   }
@@ -7106,12 +7104,12 @@ Resource.prototype.removeListener = function (name, fn) {
 };
 
 Resource.prototype.removeListeners = function () {
-  this._listeners.save = [];
-  this._listeners.remove = [];
+  this.$listeners.save = [];
+  this.$listeners.remove = [];
 };
 
 Resource.prototype.disconnect = function () {
-  this._socket.disconnect();
+  this.$socket.disconnect();
 };
 
 module.exports = Resource;
